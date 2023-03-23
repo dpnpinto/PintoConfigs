7e0ffbdeac3a9653c134b7fe92121b159fc8b63f
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2023-03-23 11:53:09 -0100

    Update placa_rede.sh

--- placa_rede.sh
+++ placa_rede.sh
@@ -1,2 +1,20 @@
-lspci -v | less
+# dispositivos pci
+lspci -v | less 
+# detalhe da placa d erede
 sudo dmesg | grep e1000
+# endereço da placa de rede
+ip addr
+# ifconfig foi descontinuado pode ser instalado pacman -S net-tools (não aconselhavel por estar descontinuado)
+# listar as interfaces do meu sistema
+ls /sys/class/net
+# endereço mac da minha interface
+cat /sys/class/net/"nome da interface"/address
+#todas as defenições
+ls /sys/class/net/"nome da interface" 
+# ativar ou desativar interface
+ip link set "nome da interface" down/up
+# pedir novo dhcp
+dhcpcd "nome da inerface"
+# Ativar/desativar o serviço de shcp no arch
+sudo  systemctl enable/disable dhcpcd.servide
+# utilização do net clontrol "netctl" para criação de perfis
