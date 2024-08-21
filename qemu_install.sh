14924ab49cc81f6cb6f2513d3445818208a19905
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-08-21 16:03:22 +0000

    Update qemu_install.sh

--- qemu_install.sh
+++ qemu_install.sh
@@ -1,11 +1,9 @@
 sudo pacman -Syy
 sudo pacman -S archlinux-keyring
 sudo pacman -S qemu-full libvirt virt-manager
+systemctl enable libvirtd.socket
+systemctl start libvirtd.socket
 
-sudo pacman -S ebtables iptables
-sudo pacman -S libguestfs
-sudo systemctl enable libvirtd.service
-sudo systemctl start libvirtd.service
 # sudo pacman -S vim
 sudo vim /etc/libvirt/libvirtd.conf
 #unix_sock_group = "libvirt"
