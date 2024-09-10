32a96be382bae3006f77739e692ed460a77ec73f
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-10 23:53:43 +0000

    Update qemu_install.sh

--- qemu_install.sh
+++ qemu_install.sh
@@ -2,11 +2,11 @@ sudo pacman -Syy
 sudo pacman -S archlinux-keyring
 sudo pacman -S qemu-full libvirt virt-manager
 sudo systemctl enable libvirtd.socket
-sudo systemctl start libvirtd.socket
+sudo systemctl start libvirtd.socket # use socket nop need to define permissions
 # sudo pacman -S vim
-sudo vim /etc/libvirt/libvirtd.conf
-#unix_sock_group = "libvirt"
-#unix_sock_rw_perms = "0770"
-sudo usermod -a -G libvirt $(whoami)
-newgrp libvirt
-sudo systemctl restart libvirtd.service
+# sudo vim /etc/libvirt/libvirtd.conf
+# unix_sock_group = "libvirt"
+# unix_sock_rw_perms = "0770"
+# sudo usermod -a -G libvirt $(whoami)
+# newgrp libvirt
+# sudo systemctl restart libvirtd.service
