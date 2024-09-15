1d9dd5368a79148a56a5bbae6d3d15b3ac594594
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-15 20:36:20 +0000

    Update qemu_install.sh

--- qemu_install.sh
+++ qemu_install.sh
@@ -2,11 +2,11 @@ sudo pacman -Syy
 sudo pacman -S archlinux-keyring
 sudo pacman -S qemu-full libvirt virt-manager
 sudo systemctl enable libvirtd.socket
-sudo systemctl start libvirtd.socket # use socket nop need to define permissions
-# sudo pacman -S vim
-# sudo vim /etc/libvirt/libvirtd.conf
-# unix_sock_group = "libvirt"
-# unix_sock_rw_perms = "0770"
-# sudo usermod -a -G libvirt $(whoami)
-# newgrp libvirt
-# sudo systemctl restart libvirtd.service
+sudo systemctl start libvirtd.socket # use socket for wokstation you do not need to define permission
+# Setup you network as a bdidge to conect all VM and easy acess
+nmcli con show # To see you conections
+nmcli con add type bridge ifname br0 con-name br0-con # to add a bridge named br0 with conection br0-con
+nmcli con add type bridge-slave ifname "you network interface name" master br0 # add your fisical network as  master interface of your br0
+nmcli con up br0-con  # bring bridge up
+nmcli con down "The other conection" # put the other down
+sudo nmcli con modify br0 bridge.stp no # disable stp in the bridge 
