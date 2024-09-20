d52a060fc94e9556e7975e64f70da5fa32f89ece
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-20 15:45:51 +0000

    Update qemu_install.sh

--- qemu_install.sh
+++ qemu_install.sh
@@ -4,6 +4,7 @@ sudo pacman -S qemu-full libvirt virt-manager
 sudo systemctl enable libvirtd.socket
 sudo systemctl start libvirtd.socket # use socket for wokstation you do not need to define permission
 # Setup you network as a bdidge to conect all VM and easy acess
+# Create a bridge out for the Qemu. Then you can conect you VM's to that bridge and manage them on your local network with the same subnet.
 nmcli con show # To see you conections
 nmcli con add type bridge ifname br0 con-name br0-con # to add a bridge named br0 with conection br0-con
 nmcli con add type bridge-slave ifname "you network interface name" master br0 # add your fisical network as  master interface of your br0
