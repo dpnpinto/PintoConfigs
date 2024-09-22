c1d6fdab792b82f8a4e8ccd08e71bda9b77136d9
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-22 23:24:05 +0000

    Update install_cups.sh

--- install_cups.sh
+++ install_cups.sh
@@ -1,7 +1,7 @@
 
 
 # install cups
-sudo pacman -S cups
+sudo pacman -S cups cups-pdf
 # I like it instaled by socket to only run when i need it then
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
