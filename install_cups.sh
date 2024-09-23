e2c0e381029c2bb6e61a4f93c9217b60d1d32463
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-23 22:02:36 +0000

    Update install_cups.sh

--- install_cups.sh
+++ install_cups.sh
@@ -1,11 +1,11 @@
 
 
 # install cups
-sudo pacman -S cups cups-pdf
+sudo pacman -S cups
 # add current user to lp (local print) group
 sudo usermod -aG lp $USER
 # I like it instaled by socket to only run when i need it then
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
-# Install a visual configuration of the printer
+# Install a GUI for the printer management
 sudo pacman -S system-config-printer
