122255a4d08cbb21892355ec0a1458775b5ebd9a
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-23 15:05:02 +0000

    Update install_cups.sh

--- install_cups.sh
+++ install_cups.sh
@@ -2,6 +2,8 @@
 
 # install cups
 sudo pacman -S cups cups-pdf
+# add current user to lp (local print) group
+sudo usermod -aG lp $USER
 # I like it instaled by socket to only run when i need it then
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
