03ae4390e785fb73ec1798919ff454aee7913ba9
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-23 15:08:59 +0000

    Update install_cups.sh

--- install_cups.sh
+++ install_cups.sh
@@ -7,4 +7,5 @@ sudo usermod -aG lp $USER
 # I like it instaled by socket to only run when i need it then
 sudo systemctl enable cups.socket
 sudo systemctl start cups.socket
-
+# Install a visual configuration of the printer
+sudo pacman -S system-conmfig-printer
