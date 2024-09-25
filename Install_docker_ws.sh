bd9aa5b88f7016df411eb46bb4e418f83cc2ca63
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-09-25 22:57:30 +0000

    Create Install_docker_ws.sh

---
+++ Install_docker_ws.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installl Docker in a Arch Linux workstation 
+sudo pacman -S docker
+# As it is a Workstation just start de socket
+sudo systemctl enable docker.socket
+sudo systemctl start docker.socket
+# just add the user to docker goup
+newgrp docker
+sudo usermod -aG docker $USER
