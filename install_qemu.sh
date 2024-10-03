677f3436b72aeddc874a8bd0b690ec3fd35ec6a7
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-10-03 14:11:51 +0000

    Update install_qemu.sh

--- install_qemu.sh
+++ install_qemu.sh
@@ -10,7 +10,7 @@ sudo systemctl start libvirtd.socket # use sockets for wokstation you do not nee
 # Create a bridge out for the Qemu. Then you can conect you VM's to that bridge and manage them on your local network with the same subnet.
 nmcli con show # To see you conections
 nmcli con add type bridge ifname br0 con-name br0-con # to add a bridge named br0 with conection br0-con
-nmcli con add type bridge-slave ifname "you network interface name" master br0 # add your fisical network as  master interface of your br0
+nmcli con add type bridge-slave ifname "you network interface name" master br0 con-name br0-mslave # add your fisical network as  master interface of your br0
 nmcli con modify br0-con bridge.stp no # disable stp in the bridge to get fast dhcp config of the 
-nmcli con up "you bridge-slave conection"  # bring bridge slave conection up
+nmcli con up br0-mslave  # bring bridge slave conection up
 nmcli con delete "The defaul fisical conection" # crean the other conection
