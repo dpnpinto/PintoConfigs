5d737612877235088ca8ccdfacc0f19bc4deeccb
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-12-08 19:04:23 -0100

    Create install_darkmode.sh

---
+++ install_darkmode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Adk mode by by dpnpinto.github.io 
+#install in both gtk and qt
+#first install manager
+sudo pacman -S lxappearance qt6ct
+# insall same template to gtk an qt
+sudo pacman  -S breeze breeze-gtk
+# config qt variable
+echo 'export QT_QPA_PLATFORMTHEME="gt6ct"' >> /etc/environment
