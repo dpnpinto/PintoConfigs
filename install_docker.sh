acca6c865f286789a7032b725c7b99e7b73aa571
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2025-08-22 18:54:30 +0000

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Installl Docker in a Arch Linux workstation 
+#By https://dpnpinto.github.com
+sudo pacman -S docker
+# As it is a Workstation just start from socket
+sudo systemctl enable docker.socket
+sudo systemctl start docker.socket
+# just add the user to docker goup
+newgrp docker
+sudo usermod -aG docker $USER
+# then install docker compose
+sudo pacman -S docker-compose
