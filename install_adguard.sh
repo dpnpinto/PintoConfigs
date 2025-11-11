87f76061478f88ac58f50988fa884d2c264614de
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2025-11-11 13:13:23 +0000

    Update install_adguard.sh

--- install_adguard.sh
+++ install_adguard.sh
@@ -1,3 +1,3 @@
 # Instalation os ADGuard must have curl instaled
-su
+# To remove just -> sudo /opt/AdGuardHome/AdGuardHome -s uninstall
 curl -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
